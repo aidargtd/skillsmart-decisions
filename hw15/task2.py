1e6f67235dc690e2d9cb2362c37a86dd8ae69852
Aidar Karimov <karimovgtd@yandex.ru>
2023-12-23 18:19:12 +0300

    edit decision

--- hw15/task2.py
+++ hw15/task2.py
@@ -5,14 +5,14 @@ arr = [random.randint(1, 10) for _ in range(100)]
 
 def f(values, n):
     d = {}
-    ans = []
+    ans = set()
     for i in values:
         if d.get(i):
             d[i] += 1
+            if d[i] >= n:
+                ans.add(i)
         else:
             d[i] = 1
-    for j in d.keys():
-        if d[j] >= n:
-            ans.append(j)
     return ans
 
+print(f(arr, 10))
