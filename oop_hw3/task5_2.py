b401ea61fe40c1e12a580064d83183f7b775c336
Aidar Karimov <karimovgtd@yandex.ru>
2023-12-23 17:54:26 +0300

    edit decision oop_hw3

--- oop_hw3/task5_2.py
+++ oop_hw3/task5_2.py
@@ -2,15 +2,16 @@
 5.2. Постройте две небольшие и косвенно логически связанные иерархии классов в вашей программе
 (например, Животное - Кот/Собака, и Переноска животных - Сумка для котика/Чемодан для собаки).
 Не выдумывайте никакие абстрактные сущности, только запутаетесь. Возьмите простые физические вещи,
-например, автомобиль и двигатель, тарелка и еда, кошелёк и деньги и т. п..
+например, автомобиль и двигатель, тарелка и еда, кошелёк и деньги и т. п.
 У родительского класса в каждой иерархии должно быть не менее двух наследников.
-В каждом дочернем классе должно быть не менее двух оригинальных методов, характеризующих уникальность
+В каждом дочернем классе должно быть не менее двух оригинальных методов,
+характеризующих уникальность
 этих классов, их отличие от родительского.
 """
 
 
-# Родительский класс - чай
 class Tea:
+    """Родительский класс - чай"""
     def __init__(self, tea_type, origin, color, brew_method, percent_of_caffeine, type_leaves):
         self.type_of_tea = tea_type
         self.origin = origin
@@ -20,94 +21,117 @@ class Tea:
         self.type_of_leaves = type_leaves
 
 
-# Дочерний класс для класса "чай", Черный чай
 class BlackTea(Tea):
-    def __init__(self, tea_type, origin, color, brew_method, percent_of_caffeine, type_leaves, strength):
-        super().__init__(tea_type, origin, color, brew_method, percent_of_caffeine, type_leaves)
+    """Дочерний класс для класса "чай", Черный чай"""
+    def __init__(self, tea_type, origin, color, brew_method,
+                 percent_of_caffeine, type_leaves, strength):
+        super().__init__(tea_type, origin, color, brew_method,
+                         percent_of_caffeine, type_leaves)
         self.strength = strength  # Уровень крепости чая(0/10)
+        self.TIME_SEC_ZERO_STRENGTH_TEA = 0
+        self.TIME_SEC_LOW_STRENGTH_TEA = 80
+        self.TIME_SEC_MEDIUM_STRENGTH_TEA = 120
+        self.TIME_SEC_HARD_STRENGTH_TEA = 180
 
-    def determine_brew_time(self):
+    def determine_brew_time_in_seconds(self):
         if self.strength == 0:
-            return 'Можете не заваривать'
+            return self.TIME_ZERO_STRENGTH_TEA_SEC
         elif 0 < self.strength < 4:
-            return 'Заваривайте чай 1 минуту 20 сек'
+            return self.TIME_SEC_LOW_STRENGTH_TEA
         elif 3 < self.strength < 7:
-            return 'Заваривайте чай 2 минуты'
+            return self.TIME_SEC_MEDIUM_STRENGTH_TEA
         else:
-            return 'Заваривайте чай 3 минуты'
+            return self.TIME_SEC_HARD_STRENGTH_TEA
 
     def add_milk(self):
         if self.strength <= 7:
-            return 'Можете добавить немного молока по желанию, ' \
-                   'только добавьте к времени заварки 10-20 секунд в зависимости от крпкости'
+            return True
         else:
-            return 'При добавлении молока ваша желаемая терпкость не сохранится, так что не стоит его добавлять'
+            return False
 
 
-# Дочерний класс для класса "чай", Зеленый чай
 class Puer(Tea):
+    """Дочерний класс для класса "чай", Зеленый чай"""
+
     def __init__(self, tea_type, origin, color, brew_method, percent_of_caffeine, type_leaves, aging_years,
                  toppings=False):
         super().__init__(tea_type, origin, color, brew_method, percent_of_caffeine, type_leaves)
         self.aging_years = aging_years  # Возраст пуэра в годах
         self.recommendation_toppings = toppings  # Запрашивает значение True or False
+        self.TOPPING_GINGER = 1
+        self.TOPPING_STRAWBERRY = 2
+        self.TOPPING_HONEY = 3
+        self.TOPPING_RASPBERRY = 4
 
-    def calculate_cost_of_puer(self):
+    def calculate_cost_of_puer_dollars(self):
         # Пример формулы для расчета рейтинга на основе возраста и процента содержания кофеина
         rating = ((self.aging_years * 0.1) + self.caffeine_content) / 15
         # Пусть рейтинг * 50$ = цена за 100 грамм чая
-        return f"Цена чая: {rating * 50}$ за сто грамм"
+        return rating * 50
 
-    def show_recommendations_for_drink(self):
-        if self.recommendation_toppings == True:
-            return 'Пуэр очень хорошо сочетается с ягодами(малина, клубника, шиповник), а также мятой и имбирем.'
+    def add_topping_for_drink(self, topping_num):
+        "Если метод возвращает True, значит человек добавил топинг, иначе False"
+        if topping_num == self.TOPPING_GINGER:
+            return True
+        elif topping_num == self.TOPPING_STRAWBERRY:
+            return True
+        elif topping_num == self.TOPPING_HONEY:
+            return True
+        elif topping_num == self.TOPPING_RASPBERRY:
+            return True
         else:
-            return 'Приятного чаепития!'
+            return False
 
 
-# Родительский класс - аксессуар для чая
 class TeaAccessory:
+    """Родительский класс - аксессуар для чая"""
+
     def __init__(self, name, material):
         self.name = name
         self.material = material
 
 
-# Дочерний класс для класса "аксессуар для чая", Заварочный чайник
 class Teapot(TeaAccessory):
+    """Дочерний класс для класса "аксессуар для чая", Заварочный чайник"""
+
     def __init__(self, name, material, volume, tea_amount):
         super().__init__(name, material)
         self.volume = volume
         self.tea_amount = tea_amount
 
     def calculate_water_to_tea_ratio(self):
-        # Рассчитываем соотношение воды к количеству чая
+        """Рассчитываем соотношение воды к количеству чая"""
         water_to_tea = self.volume / self.tea_amount
         if 40 < water_to_tea < 60:
-            return 'Все в норме!'
+            return True
         else:
-            return 'Надо поменять соотношения'
+            return False
 
     def detect_bad_material(self):
+        """Если вернулось значение True, значит материал плохой"""
         if self.material.lower() == 'пластик':
-            return 'Пластик при нагревании выделяет плохие вещества, поменяйте заварочный чайник!!!'
+            return True
         else:
-            return 'Все в норме!'
+            return False
 
 
-# Дочерний класс для класса "аксессуар для чая", Чашка
 class Teacup(TeaAccessory):
+    """Дочерний класс для класса "аксессуар для чая", Чашка"""
+
     def __init__(self, name, material, volume):
         super().__init__(name, material)
         self.volume = volume
 
     def detect_bad_material(self):
+        """Если вернулось значение True, значит материал плохой"""
         if self.material.lower() == 'пластик':
-            return 'Пластик при нагревании выделяет плохие вещества, поменяйте заварочный чайник!!!'
+            return True
         else:
-            return 'Все в норме!'
+            return False
 
     def detect_bad_volume(self):
+        """Если вернулось значение True, значит объем хороший"""
         if 150 <= self.volume <= 400:
-            return 'Все в норме!'
+            return True
         else:
-            return 'Ваша кружка не подходит для идеального наслаждения чаем'
+            return False
