58f24393880fda7a710cd1df36d9e8e99981130f
Aidar Karimov <karimovgtd@yandex.ru>
2023-12-24 12:10:34 +0300

    edit decision

--- hw15/task2.py
+++ hw15/task2.py
@@ -9,10 +9,13 @@ def f(values, n):
     for i in values:
         if d.get(i):
             d[i] += 1
-            if d[i] >= n:
+            if d[i] == n:
                 ans.add(i)
+            elif d[i] > n and (i in ans):
+                ans.remove(i)
         else:
             d[i] = 1
     return ans
+
 
 print(f(arr, 10))
