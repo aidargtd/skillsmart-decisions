3b26ab0165cec6b5dd3605d167a3fdd4f2659774
Aidar Karimov <karimovgtd@yandex.ru>
2023-12-31 13:05:51 +0300

    edit decision

--- ASD_course/first_hw/all_tasks.py
+++ ASD_course/first_hw/all_tasks.py
@@ -1,5 +1,5 @@
-"""Tasks 1.1 - 1.8 without 1.7"""
-
+"""Tasks 1.1 - 1.8"""
+import unittest
 
 class Node:
     def __init__(self, v):
@@ -110,3 +110,73 @@ def f(s1, s2):
             node1 = node1.next
             node2 = node2.next
     return ans
+
+
+
+class Tests(unittest.TestCase):
+    def test_del(self):
+        s_list = LinkedList()
+
+        s_list.delete(10)
+        self.assertIsNone(s_list.head)
+        self.assertIsNone(s_list.tail)
+
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        s_list.delete(1)
+        self.assertEqual(s_list.head.value, 2)
+
+        s_list.add_in_tail(Node(4))
+        s_list.add_in_tail(Node(4))
+        s_list.delete(4, all=True)
+        self.assertEqual(s_list.head.value, 2)
+        self.assertIsNone(s_list.head.next)
+
+        s_list.delete(2)
+        self.assertIsNone(s_list.head)
+        self.assertIsNone(s_list.tail)
+
+    def test_clean_list(self):
+        s_list = LinkedList()
+
+        s_list.clean()
+        self.assertIsNone(s_list.head)
+        self.assertIsNone(s_list.tail)
+
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        s_list.clean()
+        self.assertIsNone(s_list.head)
+        self.assertIsNone(s_list.tail)
+
+    def test_find_all(self):
+        s_list = LinkedList()
+
+        self.assertEqual(len(s_list.find_all(1)), 0)
+
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(0))
+        s_list.add_in_tail(Node(0))
+        self.assertEqual(len(s_list.find_all(0)), 2)
+
+    def test_len(self):
+        s_list = LinkedList()
+        self.assertEqual(s_list.len(), 0)
+        s_list.add_in_tail(Node(1))
+        s_list.add_in_tail(Node(2))
+        s_list.add_in_tail(Node(3))
+        self.assertEqual(s_list.len(), 3)
+
+    def test_insert(self):
+        s_list = LinkedList()
+        s_list.insert(None, Node(1))
+        self.assertEqual(s_list.head.value, 1)
+        self.assertEqual(s_list.tail.value, 1)
+
+        s_list.insert(1, Node(2))
+        self.assertEqual(s_list.head.next.value, 2)
+        self.assertEqual(s_list.tail.value, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
